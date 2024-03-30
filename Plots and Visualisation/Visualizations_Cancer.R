1c923686bd6313bbdb2c3360a4e400ddd2b4dca3
swapnonilmondal <98679771+swapnonilmondal@users.noreply.github.com>
2024-03-30 20:59:18 +0530

    Update Visualizations_Cancer.R

--- Plots and Visualisation/Visualizations_Cancer.R
+++ Plots and Visualisation/Visualizations_Cancer.R
@@ -2,31 +2,46 @@
 ########### Age Specific Cancer Incidence Visualizations With  Gender ##########
 #==============================================================================#
 
-# Class Limits are : 0-14, 15-39, 40-64, 65+
+# 0-14, 15-39, 40-64, 65+
 
 FRE_M <- c(21308, 74872, 341230, 274766)
 dma <- rep(c(15,40,65,80), FRE_M)
 #table(cut(dma, breaks = c(0,15,40,65,90), right = TRUE))
-#hist(dma, breaks = c(0,15,40,65,80))
-plot(density(dma, bw = 8), xlim = c(3.8,100), ylim = c(0.00105, 0.035),
+hist(dma, breaks = c(0,15,40,65,80), , xlim = c(3.8,100), ylim = c(0.00105, 0.035),
+     main = "Age-wise Male Cancer Incidence Density",
+     xlab = "Age")
+lines(density(dma, bw = 8), xlim = c(3.8,100), ylim = c(0.00105, 0.035),
      main = "Age-wise Male Cancer Incidence Density",
      xlab = "Age")
 
+#density(dma, bw = 2)$x
+
 FRE_F <- c(13709, 94166, 425918, 215458)
 dfe <- rep(c(15,40,65,80), FRE_F)
-plot(density(dfe, bw = 8), xlim = c(3.8,100), ylim = c(0.00105, 0.035),
+hist(dfe, breaks = c(0,15,40,65,80), , xlim = c(3.8,100), ylim = c(0.00105, 0.035),
      main = "Age-wise Female Cancer Incidence Density",
      xlab = "Age")
+lines(density(dfe, bw = 8), xlim = c(3.8,100), ylim = c(0.00105, 0.035),
+     main = "Age-wise Female Cancer Incidence Density",
+     xlab = "Age")
+
+#density(dfe)$x
 
 #==============================================================================#
 ################ Gender wise top ten cancer sites Visualizations ###############
 #==============================================================================#
 
 Site_1 <- read.csv("C:/Users/Swapnonil/Documents/Site_M_F_Cancer.csv")
+Site_1$M_Prop <- paste(as.character(round(Site_1$Male/sum(Site_1$Male) * 100, 2)),
+                       "%", sep = "")
+Site_1$F_Prop <- paste(as.character(round(Site_1$Female/sum(Site_1$Female) * 100, 2)),
+                       "%", sep = "")
 Site <- Site_1[-54,]
+#Site
 
-M_Site_ord <- M_Site[order(Site$Male, decreasing = FALSE),]
-F_Site_ord <- M_Site[order(Site$Female, decreasing = FALSE),]
+M_Site_ord <- Site[order(Site$Male, decreasing = FALSE),]
+F_Site_ord <- Site[order(Site$Female, decreasing = FALSE),]
+
 m_t <- tail(M_Site_ord, 10)
 f_t <- tail(F_Site_ord, 10)
 #par(mar=c(4.5,11,6,4))
@@ -41,13 +56,19 @@ library(ggplot2)
 
 ggplot(m_t, aes(x = Male, y = reorder(Site, Male))) +
   geom_bar(stat = "identity", fill = "cornflowerblue") +
+  geom_text(aes(label = M_Prop), color = "black",
+            position = position_stack(vjust = 1), hjust = -0.20) +
   labs(title = "Top Ten Cancer Sites of Male",
-       y = "", x = "Frequency")
+       y = "", x = "Frequency") +
+  xlim(0, 200000)
 
 ggplot(f_t, aes(x = Female, y = reorder(Site, Female))) +
   geom_bar(stat = "identity", fill = "deepskyblue3") +
+  geom_text(aes(label = F_Prop), color = "black",
+            position = position_stack(vjust = 1), hjust = -0.20) +
   labs(title = "Top Ten Cancer Sites of Female",
-       y = "", x = "Frequency")
+       y = "", x = "Frequency") +
+  xlim(0, 240000)
 
 #==============================================================================#
 ################ Different type of Cancer Sites Visualization ##################
@@ -65,6 +86,8 @@ colnames(Can_lab_tab) <- c("Types", "Freq", "Prop")
 
 ggplot(Can_lab_tab, aes(x = Types, y = Freq)) +
   geom_bar(stat = "identity", fill = "cornflowerblue") +
+  geom_text(aes(label = Prop), color = "white",
+            position = position_stack(vjust = c(0.5))) +
   labs(title = "Different Types of Cancer Sites",
        y = "Frequency", x = "Types")
 
